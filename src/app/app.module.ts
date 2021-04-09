2213a639ed80370ab412d72e5da65a17b7806cd2
Alec Pagliarussi <alec.paglia@gmail.com>
2021-04-09 18:37:32 -0400

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MissionlistComponent} from './missionlist/missionlist.component';
-import {HttpClientModule} from '@angular/common/http';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MissionlistComponent } from './missionlist/missionlist.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardComponent } from './card/card.component';
 import { MissionComponent } from './mission/mission.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { MissionComponent } from './mission/mission.component';
     MissionlistComponent,
     NavbarComponent,
     CardComponent,
-    MissionComponent
+    MissionComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,9 +34,9 @@ import { MissionComponent } from './mission/mission.component';
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
